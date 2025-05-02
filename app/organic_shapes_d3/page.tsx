1d4efdc16ee15d36567f82124d922c6dff95e27a
Nastasja Steinhauer <nastasja.steinhauer@gmx.de>
2025-05-02 15:04:44 +0200

    refactor: added layout and page, reorganized folder

---
+++ app/organic_shapes_d3/page.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import Head from "next/head";
+
+const Page = () => {
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
+export default Page;
---
+++ styles/theme.ts
@@ -0,0 +1,56 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#b8a9a3", // beige
+    },
+    secondary: {
+      main: "#00FF85", // green
+      light: "rgba(0, 255, 133, 0.3)",
+    },
+    districts: {
+      main: "#000042", // blue
+    },
+    charts: {
+      main: "green",
+    },
+    text: {
+      primary: "#000042", // blue
+      secondary: "#666666",
+    },
+    background: {
+      default: "#b8a9a3",
+    },
+  },
+  typography: {
+    fontFamily: "'Avenir'",
+    h1: {
+      fontSize: "2.2rem",
+      fontWeight: 200,
+      fontFamily: "'Cormorant Infant'",
+    },
+    h2: {
+      fontSize: "3.5rem",
+      fontWeight: 300,
+      fontFamily: "'Cormorant Infant'",
+    },
+    body1: {
+      fontSize: "1.1rem",
+      fontWeight: 400,
+      fontFamily: "'Avenir'",
+      "@media (max-width:600px)": {
+        fontSize: "0.8rem",
+      },
+    },
+    body2: {
+      fontSize: "1.2rem",
+      fontWeight: 500,
+      fontFamily: "'Avenir'",
+    },
+  },
+});
+
+export default theme;
